c18831c0dcd30c0ff1edd679d328086995376c46
Luda Diepgrond <ludadiepgrond@gmail.com>
2016-12-06 18:58:02 +0100

    Particles

--- Vuforia_p2/Assets/triggerDetectedScript.cs
+++ Vuforia_p2/Assets/triggerDetectedScript.cs
@@ -21,12 +21,18 @@ namespace Vuforia {
 		// find the GO's using a coroutine								CURRENTLY NOT IN USE
 		private IEnumerator coroutine;
 
-
+		// Different effects
 		public GameObject RainEffect;
-// INSERT MORE EFFECTS like the RainEffect;
+		public GameObject PaisleyBigEffect;
+		public GameObject PaisleySmallEffect;
+		public GameObject PaisleyFlowerEffect;
+		public GameObject LightsGlitter;
+		public GameObject LightsGlitterHand;
+		public GameObject LightsLTL;
+		public GameObject LightsLTLRot;
+		public GameObject LightsSparkles;
+		public GameObject DustStorm; 
 
-
-	
 
 		void Start () {
 			print ("Start IS RUN");
@@ -67,9 +73,9 @@ namespace Vuforia {
 				// if there is a detection (== true) of the current marker in the foreach loop:
 				if (currentStatus) { 
 
-					if (markerON != marker.name) {						// ADD THIS IN WHEN MORE EFFECT ARE ADDED: && (marker.name != "Hand")
-						// New marker has been detected					// kept out for testing purposes, but we do want to hand to run 
-						resetEffect (markerON);							// simultaneously with other effects
+					if (markerON != marker.name && (marker.name != "Hand")) {						// ADD THIS IN WHEN MORE EFFECT ARE ADDED: && (marker.name != "Hand")
+						// New marker has been detected												// kept out for testing purposes, but we do want to hand to run 
+						resetEffect (markerON);														// simultaneously with other effects
 					}														
 
 					// Which marker is it?
@@ -88,21 +94,106 @@ namespace Vuforia {
 						triggered = true;
 						break;
 
-// add more cases   case "Trigger2":
-// like this			//Run visual effect
-//						//Only if run first time it is detected...
-//						if (!triggered) {
-//							//INSERT CODE HERE TO RUN EFFECT
-//							print ("Running effect: NAME");
-//							// Store the marker which is ON
-//							markerON = marker.name;
-//							print ("MarkerON: " + markerON);
-//						}
-//						triggered = true;
-//						break;
+				   case "Trigger2":
+
+						if (!triggered) {
+							PaisleyBigEffect.SetActive (true);
+							print ("Running effect: PAISLEY_BIG");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger3":
+
+						if (!triggered) {
+							PaisleySmallEffect.SetActive (true);
+							print ("Running effect: PAISLEY_SMALL");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger4":
+
+						if (!triggered) {
+							PaisleyFlowerEffect.SetActive (true);
+							print ("Running effect: PAISLEY_FLOWER");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger5":
+
+						if (!triggered) {
+							LightsGlitter.SetActive(true);
+							print ("Running effect: GLITTER");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger6":
+
+						if (!triggered) {
+							LightsLTL.SetActive(true);
+							print ("Running effect: LTL");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger7":
+
+						if (!triggered) {
+							LightsLTLRot.SetActive(true);
+							print ("Running effect: LTL_ROT");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger8":
+
+						if (!triggered) {
+							LightsSparkles.SetActive(true);
+							print ("Running effect: SPARKLES");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
+					case "Trigger9":
+
+						if (!triggered) {
+							DustStorm.SetActive(false);
+							print ("Running effect: DUST_STORM");
+							// Store the marker which is ON
+							markerON = marker.name;
+							print ("MarkerON: " + markerON);
+						}
+						triggered = true;
+						break;
+
 
 					case "Hand":
 						//Run visual effect
+						LightsGlitterHand.SetActive(true);
 						//Runs automatically because of Vuforia, only while detection is valid
 						print ("Running effect FROM HAND");
 						break;
@@ -125,6 +216,15 @@ namespace Vuforia {
 			print ("Disabling all effects on start");
 
 			RainEffect.SetActive(false);
+			PaisleyBigEffect.SetActive(false);
+			PaisleySmallEffect.SetActive(false);
+			PaisleyFlowerEffect.SetActive(false);
+			LightsGlitter.SetActive(false);
+			LightsGlitterHand.SetActive (false);
+			LightsLTL.SetActive(false);
+			LightsLTLRot.SetActive(false);
+			LightsSparkles.SetActive(false);
+			DustStorm.SetActive(false);
 
 		}
 
@@ -140,10 +240,37 @@ namespace Vuforia {
 				break;
 
 
-// INSERT	case "Trigger2":
-//				RainEffect.SetActive (false);
-//				break;
+			case "Trigger2":
+				PaisleyBigEffect.SetActive (false);
+				break;
 
+			case "Trigger3":
+				PaisleySmallEffect.SetActive (false);
+				break;
+
+			case "Trigger4":
+				PaisleyFlowerEffect.SetActive (false);
+				break;
+
+			case "Trigger5":
+				LightsGlitter.SetActive (false);
+				break;
+
+			case "Trigger6":
+				LightsLTL.SetActive (false);
+				break;
+
+			case "Trigger7":
+				LightsLTLRot.SetActive (false);
+				break;
+
+			case "Trigger8":
+				LightsSparkles.SetActive (false);
+				break;
+
+			case "Trigger9":
+				DustStorm.SetActive (false);
+				break;
 
 			default:
 				break;
